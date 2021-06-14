5ac5d6b2a64cf6d8310db118f5ca5163a9cc5e06
Tomas <t.e.muller-bravo@soton.ac.uk>
2021-06-14 10:15:42 +0100

    update and correct tests

--- tests/test_tessreduce_main.py
+++ tests/test_tessreduce_main.py
@@ -1,4 +1,4 @@
-# test the main function in TESSreduce
+# test the main functions in TESSreduce
 
 import unittest
 import tessreduce as tr
@@ -8,15 +8,7 @@ class TestTESSreduce(unittest.TestCase):
     self.ra = 189.1385817
     self.dec = 11.2316535
     self.tess = tr.tessreduce(ra=self.ra, dec=self.dec)
-
-    def test_Centroids_DAO(self):
-
-        self.tess.Centroids_DAO()
-        self.assertTrue()
-
-    def test_Shift_images(self):
-
-        self.tess.Shift_images()
+    self.tess.get_ref()
 
     def test_Make_mask(self):
 
@@ -26,9 +18,21 @@ class TestTESSreduce(unittest.TestCase):
 
         self.tess.background()
 
+    def test_Centroids_DAO(self):
+
+        self.tess.Centroids_DAO()
+
+    def test_Shift_images(self):
+
+        self.tess.Shift_images()
+
     def test_field_calibrate(self):
 
         self.tess.field_calibrate()
+
+    def test_diff_lc(self):
+
+        self.tess.Diff_lc()
 
 if __name__ == '__main__':
     unittest.main()
