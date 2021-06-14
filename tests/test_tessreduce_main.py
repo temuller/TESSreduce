8141803bae06dc4d8303d4865b4f9a5f721b20ac
Tomas <t.e.muller-bravo@soton.ac.uk>
2021-06-14 11:07:24 +0100

    add suite to tests

--- tests/test_tessreduce_main.py
+++ tests/test_tessreduce_main.py
@@ -12,28 +12,33 @@ class TestTESSreduce(unittest.TestCase):
         self.tess.get_ref()
 
     def test_Make_mask(self):
-
         self.tess.Make_mask()
 
     def test_background(self):
-
         self.tess.background()
 
     def test_Centroids_DAO(self):
-
         self.tess.Centroids_DAO()
 
     def test_Shift_images(self):
-
         self.tess.Shift_images()
 
     def test_field_calibrate(self):
-
         self.tess.field_calibrate()
 
-    def test_diff_lc(self):
-
+    def test_Diff_lc(self):
         self.tess.Diff_lc()
 
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(WidgetTestCase('test_Make_mask'))
+    suite.addTest(WidgetTestCase('test_background'))
+    suite.addTest(WidgetTestCase('test_Centroids_DAO'))
+    suite.addTest(WidgetTestCase('test_Shift_images'))
+    suite.addTest(WidgetTestCase('test_field_calibrate'))
+    suite.addTest(WidgetTestCase('test_Diff_lc'))
+    return suite
+
 if __name__ == '__main__':
-    unittest.main()
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
